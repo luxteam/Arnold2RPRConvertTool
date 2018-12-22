19f3590aa976c478b5d6ed9ed0f7657510186d9b
Andrey Kravchenko <epozine@inbox.ru>
2018-12-22 20:00:56 +0300

    v.1.7 Fix bug with channel converting, fix bug with creating extra materials.

--- tools/generate_template.bat
+++
@@ -1,3 +0,0 @@
-set MAYA_SCRIPT_PATH=%cd%;%MAYA_SCRIPT_PATH%
-
-"C:\Program Files\Autodesk\Maya2018\bin\maya.exe" -command "source generate_template.mel; evalDeferred -lp (makeCommon(\"C:\\Arnold2RPR\\tools\\arnold.ma\"));"
